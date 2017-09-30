b267ab0eab8ae1ef5d5021fb85db22a04787f7a5
殷鹏飞 <pengfei.yin@china.zhaogang.com>
2017-09-30 18:02:37 +0800

    mongoose添加

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ routes(app);
 //   res.send(data);
 // });
 
-var server = app.listen(3000, function() {
+var server = app.listen(5000, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
--- server/mongo/mongoose.js
+++ server/mongo/mongoose.js
@@ -1,7 +1,23 @@
-var mongoose = require('mongoose'),
-  DB_URL = 'mongodb://127.0.0.1:27017/test';
-var db = mongoose.connection;
-db.on("error",console.error.bind(console, "connection error:"));
-db.once("open", function () {
-    // 已连接！
+var mongoose = require('mongoose'); //引用mongoose模块
+var db = mongoose.createConnection('localhost', 'test'); //创建一个数据库连接
+db.on('error', console.error.bind(console, '连接错误:'));
+db.once('open', function() {
+  console.log('数据库连接成功')
 });
+
+// 定义一个Schema
+var PersonSchema = new mongoose.Schema({
+  name: String //定义一个属性name，类型为String
+});
+
+// 将该Schema发布为Model
+var PersonModel = db.model('Per', PersonSchema);
+
+
+// 用Model创建Entity
+var personEntity = new PersonModel({ name: 'Krouky' });
+//打印这个实体的名字看看
+console.log(personEntity); //Krouky
+
+// 将数据保存的数据库，此时数据库便有数据了
+personEntity.save()
